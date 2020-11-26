87bcfd658b8385de96aa7260b82a7256a97b38ea
Igor Ryabov <mr.ryaboff2013@yandex.ru>
2020-11-26 23:30:22 +0600

    Initial commit

---
+++ www/scripts/favs.js
@@ -0,0 +1,41 @@
+﻿if (localStorage.getItem('fav') === null) localStorage.setItem('fav', '');
+var favs = localStorage.getItem('fav').split(';');
+if (localStorage.getItem('fav') == '') $('#favs').text('Нет избранных аптек');
+else {
+    $('#favs').text('');
+    favs.forEach((addr) => {
+        $('#favs').append('<div><span>' + addr + '</span><button>Удалить</button></div>');
+    });
+}
+$('#favs button').click((e) => {
+    e = $(e.target);
+    let addr = $('span', e.closest('div')).text();
+    e.closest('div').remove();
+    favs.splice(favs.indexOf(addr, 1));
+    localStorage.setItem('fav', favs.join(';'));
+    location.reload();
+});
+
+$.get('https://cors.io?http://apteka-omsk.ru/our-pharmacy', (raw) => {
+    let dom = $.parseHTML(raw);
+    $('#allPharm').html('');
+    let addresses = [];
+    $('.views-row h2 a', dom).get().forEach((el) => {
+        addresses.push($(el).text());
+    });
+    console.log(addresses);
+    addresses.forEach((addr) => {
+        if (favs.indexOf(addr) == -1) $('#allPharm').append('<div><span>' + addr + '</span><button>Добавить</button></div>');
+    });
+    $('#allPharm button').click((e) => {
+        e = $(e.target);
+        let addr = $('span', e.closest('div')).text();
+        e.closest('div').remove();
+        //let cfavs = localStorage.getItem('fav').split(';');
+        if (!(favs.length == 1 && favs[0] == '')) favs.push(addr);
+        else favs = [addr];
+        localStorage.setItem('fav', favs.join(';'));
+        location.reload();
+    });
+});
+//
---
+++ www/scripts/index.js
@@ -0,0 +1,55 @@
+﻿let foundDrugs = [];
+
+$('#drugFind').click(() => {
+    foundDrugs = [];
+    $('#found').text('');
+    $('#addressInput').val('');
+    if ($('#drugInput').val().length < 1) return $('#results').html('Запрос должен быть длиннее 1-й буквы');
+    $('#results').html('Подождите...');
+    if ($('#drugInput').val() == 'тест ошибки') throw new Error('Выброшено тестовое исключение');
+    $.ajax({
+        method: 'POST',
+        url: 'https://apteka-omsk.ru/node?ajax_form=1',
+        dataType: 'JSON',
+        data: {
+            title: $('#drugInput').val(),
+            form_id: 'ajax_search'
+        },
+        success: raw => {
+            console.log(raw);
+            $('#results').html('');
+            let dom = $.parseHTML(raw[0].data.replace(/<img[^>]*>/g, "").replace(/<link[^>]*>/g, "").replace(/<style[^>]*>/g, "").replace(/<script[^>]*>/g, ""));
+            let rows = $('.result', dom).get();
+            if (!rows.length) return console.log('No rows!');
+            if (/^По вашему запросу ничего не найдено/i.test($(rows[0]).text())) return $('#results').html('Ничего не найдено');
+            if (/^В Вашем запросе должно быть более 3х букв/i.test($(rows[0]).text())) return $('#results').html('Запрос должен быть длиннее 1-й буквы');
+            rows.forEach((el) => {
+                let cols = $(el).children().get();
+                let drug = [$(cols[0]).text(), +$(cols[1]).text().replace(' руб.', ''), $(cols[2]).text().trim()];
+                foundDrugs.push(drug);
+                //console.log('Drug:', drug);
+            });
+            redraw();
+            $('#found').text('Найдено: ' + foundDrugs.length);
+            //console.log("Все строки: ", rows, "; все найденные: ", foundDrugs);
+        },
+        error: (jqXHR, eStr, err) => {
+            $('#results').html(`<b>Что-то пошло не так</b><br/>${eStr}<br/>${typeof err == 'string' ? err : (err.stack ? err.stack : '')}`);
+        }
+    });
+});
+
+function redraw() {
+    console.log('redraw called');
+    $('#results').html('');
+    let filter = $('#addressInput').val().toLowerCase();
+    foundDrugs.forEach((drug) => {
+        if (!filter || drug[2].toLowerCase().indexOf(filter) != -1) $('#results').append(`<div class=drugNormal>${drug[0]}<br/>${drug[1]} руб.<br/>${drug[2]}<hr/></div>`);
+    });
+}
+
+$('#addressFilter').on('click', () => redraw());
+
+window.onerror = (msg, url, lineNo, columnNo, error) => {
+    $('#results').html(`<b>Что-то пошло не так</b><br/>${error.stack ? error.stack : 'No stack available'}`);
+}
